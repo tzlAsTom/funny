4a667af56a102a4c7fb1fb9e3f617a1a11b10601
user <user@debian.local>
2021-08-18 13:58:42 +0800

    add tcp server

--- black-hole/blackHole.js
+++ black-hole/blackHole.js
@@ -1,5 +1,6 @@
 const cluster = require('cluster');
 const http = require('http');
+const net = require('net');
 const numCPUs = require('os').cpus().length;
 const process = require('process');
 
@@ -16,6 +17,29 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
+  const server = net.createServer( (c) => {
+      let clientId = `${c.remoteAddress}:${c.remotePort}`;
+      console.log(Date(), 'new client', clientId);
+      
+      c.on('data', (data) => {
+        console.log(Date(), clientId, 'client data str', data.toString());
+        console.log(Date(), clientId, 'client data buf', data.toString('hex'));
+      });
+      
+      c.on('end', () => {
+        console.log(Date(), clientId, 'client end');
+      });      
+  });
+  server.on('error', (err) => {
+      conosle.log('tcp server error', err);
+  });
+  server.listen(8124, () => {
+      console.log('tcp server started');
+  });
+  
+  
+  
+  
   // Workers can share any TCP connection
   // In this case it is an HTTP server
   http.createServer((req, res) => {
@@ -26,13 +50,13 @@ if (cluster.isMaster) {
         body = Buffer.concat([body, buf]);
     });
     req.on('end', () => {
-        console.log(Date(), req.socket.remoteAddress + ':' + req.socket.remotePort, req.method, req.url, JSON.stringify(req.rawHeaders));
+        console.log(Date(), 'HTTP', req.socket.remoteAddress + ':' + req.socket.remotePort, req.method, req.url, JSON.stringify(req.rawHeaders));
         console.log(body.toString('hex'));
 
         res.writeHead(200);
         res.end('hello world\n');
     });   
-  }).listen(443);
+  }).listen(8080);
 
   console.log(`Worker ${process.pid} started`);
 }
